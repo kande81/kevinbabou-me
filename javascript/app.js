8d020713692c3ac650b557b9cc4b05fbee1ef852
gilles <yannickbabou@gmail.com>
2023-01-23 12:43:57 -0800

    completed and tested the mobile view version

--- javascript/app.js
+++ javascript/app.js
@@ -3,16 +3,16 @@ const header = document.querySelector(".header");
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".nav-links li");
-const searchBar = document.getElementsByName("theme-color");
-console.log(searchBar);
 
 burger.addEventListener("click", () => {
   body.classList.toggle("bhidden");
+  if (nav.style.animation) {
+    nav.style.animation = "";
+  } else {
+    nav.style.animation = "navFadeIn .8s ease forwards";
+  }
   nav.classList.toggle("navlinks-is-active");
   header.classList.toggle("menu-is-opened");
-  searchBar.setAttribute("content", "#fff");
-  console.log(searchBar.content);
-  console.log(searchBar);
   // animate the links
   navLinks.forEach((link) => {
     if (link.style.animation) {
@@ -24,3 +24,12 @@ burger.addEventListener("click", () => {
   // animate the burger icon
   burger.classList.toggle("toggle");
 });
+
+// start of code to set the color of background of the header and body
+
+if (!body.id) {
+  body.style.background = "#fff";
+  header.style.backgroundColor = "#fff";
+}
+
+// end of code to set the color of background of the header and body
